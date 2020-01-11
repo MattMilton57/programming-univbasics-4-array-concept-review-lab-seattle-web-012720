f8ad0dfa2e00d89a00af0c10799959d23d51a8fa
Matt Milton <mattmilton57@gmail.com>
2020-01-11 23:11:11 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,20 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times do |count|
+  if array[count] == value_to_find
+    return count
+  end
+end
+nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+  new=array.sort
+  biggie=new.pop
+  return biggie
 end
 
 def find_min_value(array)
-  # Add your solution here
+    new=array.sort
+  biggie=new.shift
+  return biggie
 end
